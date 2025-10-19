db87cc07ad7c545bdadd5278e44b8ddfebdd6407
Srijon <srijonpaul003@gmail.com>
2025-10-20 02:05:29 +0530

    feat: add comment and post models, and user routes with avatar upload functionality

---
+++ 4-Blog-Api/src/models/post.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: true,
+	},
+	image: {
+		imageId: {
+			type: String,
+			required: true
+		},
+		imageUrl: {
+			type: String,
+			required: true
+		}
+	},
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true
+	},
+	likes: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}
+	],
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comment"
+		}
+	]
+},{timestamps: true})
+
+export const Post = mongoose.model("Post", postSchema);
